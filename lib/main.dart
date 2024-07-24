1fd5fab7f0ce0f84b3c1b5a457acc0ff2326090e
resulcay <resulcayop@gmail.com>
2024-07-24 16:48:14 +0300

    unsplash integration added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:unsplash_client/unsplash_client.dart';
 import 'package:win32/win32.dart';
 
 void main() {
@@ -15,16 +17,39 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Windows Wallpaper',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const HomePage(),
-    );
+    return DynamicColorBuilder(
+        builder: (ColorScheme? lightDynamic, ColorScheme? darkDynamic) {
+      return MaterialApp(
+        title: 'Windows Wallpaper',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: lightDynamic ??
+              ColorScheme.fromSwatch(primarySwatch: Colors.purple),
+        ),
+        darkTheme: ThemeData(
+          colorScheme: darkDynamic ?? const ColorScheme.dark(),
+        ),
+        themeMode: ThemeMode.system,
+        home: const HomePage(),
+      );
+    });
   }
+}
+
+final client = UnsplashClient(
+  settings: const ClientSettings(
+    credentials: AppCredentials(
+      accessKey: 'unsplash_access_key',
+      secretKey: 'unsplash_secret_key',
+    ),
+  ),
+);
+
+Future<List<Photo>> getRandomImage() async {
+  var photo = client.photos.random(count: 30).goAndGet();
+  var photoPath = photo;
+
+  return photoPath;
 }
 
 class HomePage extends StatelessWidget {
@@ -32,29 +57,45 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Windows Wallpaper'),
-      ),
-      body: MasonryGridView.count(
-        itemCount: 180,
-        crossAxisCount: 6,
-        mainAxisSpacing: 4,
-        crossAxisSpacing: 4,
-        itemBuilder: (context, index) {
-          return Tile(index: index);
-        },
-      ),
+    return FutureBuilder(
+      future: getRandomImage(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError || !snapshot.hasData) {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text('Windows Wallpaper'),
+          ),
+          body: MasonryGridView.builder(
+            mainAxisSpacing: 10,
+            crossAxisSpacing: 10,
+            gridDelegate: const SliverSimpleGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 6,
+            ),
+            itemCount: snapshot.data!.length,
+            itemBuilder: (context, index) {
+              return Tile(index: index, photo: snapshot.data![index]);
+            },
+          ),
+        );
+      },
     );
   }
 }
 
 class Tile extends StatelessWidget {
   final int index;
+  final Photo photo;
   const Tile({
     super.key,
     required this.index,
+    required this.photo,
   });
 
   @override
@@ -64,16 +105,24 @@ class Tile extends StatelessWidget {
         Navigator.of(context).push(
           MaterialPageRoute(
             builder: (context) {
-              return ImageDetailWidget(index: index);
+              return ImageDetailWidget(
+                index: index,
+                url: photo.urls.raw.toString(),
+              );
             },
           ),
         );
       },
       child: Hero(
         tag: index,
-        child: Image.network(
-          'https://picsum.photos/200/300?random=$index',
-          fit: BoxFit.cover,
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Image.network(
+            photo.urls.thumb.toString(),
+            fit: BoxFit.cover,
+          ),
         ),
       ),
     );
@@ -84,21 +133,22 @@ class ImageDetailWidget extends StatelessWidget {
   const ImageDetailWidget({
     super.key,
     required this.index,
+    required this.url,
   });
 
   final int index;
+  final String url;
 
   Future<void> downloadImage(BuildContext context, int index) async {
-    File file = await _imageOpeation(index);
+    File file = await _imageOpeation(index, url);
 
     if (context.mounted) {
       _showMessage(context, 'Downloaded successfully: ${file.path}');
     }
   }
 
-  Future<File> _imageOpeation(int index) async {
-    final url = 'https://picsum.photos/200/300?random=$index';
-    final response = await http.get(Uri.parse(url));
+  Future<File> _imageOpeation(int index, String path) async {
+    final response = await http.get(Uri.parse(path));
     final bytes = response.bodyBytes;
     final dir = await getTemporaryDirectory();
     final file = File('${dir.path}/$index.jpg');
@@ -107,7 +157,7 @@ class ImageDetailWidget extends StatelessWidget {
   }
 
   Future<void> setAsBackground(BuildContext context, int index) async {
-    File file = await _imageOpeation(index);
+    File file = await _imageOpeation(index, url);
 
     try {
       const spifUpdateinifile = 0x01;
@@ -140,7 +190,7 @@ class ImageDetailWidget extends StatelessWidget {
 
   void _showMessage(BuildContext context, message) {
     ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('Failed to set as background: $message')),
+      SnackBar(content: Text(message)),
     );
   }
 
@@ -156,10 +206,14 @@ class ImageDetailWidget extends StatelessWidget {
           children: [
             Hero(
               tag: index,
-              child: Image.network(
-                width: MediaQuery.of(context).size.width * 0.6,
-                'https://picsum.photos/200/300?random=$index',
-                fit: BoxFit.cover,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Image.network(
+                  url,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
             Positioned(
@@ -173,6 +227,7 @@ class ImageDetailWidget extends StatelessWidget {
                     },
                     child: const Text('Set as Background'),
                   ),
+                  const SizedBox(width: 10),
                   ElevatedButton(
                     onPressed: () {
                       downloadImage(context, index);
